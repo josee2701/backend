4a4becf6a7ea65e59c460e52938b4f66d2137010
Jose Campos <j.camposs2701@gmail.com>
2024-06-25 14:55:15 -0500

    Correccion de url para publicar el backend

--- config/settings.py
+++ config/settings.py
@@ -103,8 +103,8 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Agrega la URL de tu aplicación React en desarrollo
     "http://127.0.0.1:3000",
-    "https://josee2701.github.io"
-    "backend-yw41.onrender.com"
+    "https://josee2701.github.io",
+    "http://backend-yw41.onrender.com"  # Asegúrate de incluir el protocolo
     
     # Otras URL permitidas pueden ir aquí
 ]
